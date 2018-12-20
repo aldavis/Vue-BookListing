eb8fa82dc32f14f4474fab6656f4d67e643b7c1a
alex davis <alex.davis@fbitn.com>
2018-12-20 09:43:14 -0600

    module one

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -25,15 +25,16 @@ export default {
   name: 'HelloWorld',
   data() {
     return {
-      msg: 'Welcome to Your Vue.js App',
+      msg: 'Checking hot loading, looks like its working'
     };
-  },
+  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
